3436e36b89b50acb352de031e9f87d6e6c206f35
GreenCatInForest <event2016.2017@gmail.com>
2023-08-15 12:21:34 +0100

    add cheatsheet content

--- scripts/components/codeBlock.js
+++ scripts/components/codeBlock.js
@@ -1,2 +1,7 @@
-let codeBlockContainer = document.querySelectorAll(".codeBlock");
-console.log(codeBlockContainer);
+let codeBlock = document.querySelectorAll(".codeBlock");
+console.log(codeBlock);
+
+codeBlock.forEach((block) => {
+  let codeBlockLines = block.innerHTML.split("<br>");
+  console.log(codeBlockLines);
+});
