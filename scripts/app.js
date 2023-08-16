0a91407222584b81e93d57f53a9863262f62529a
GreenCatInForest <event2016.2017@gmail.com>
2023-08-16 16:12:58 +0100

    addeventlistener to main top navigation

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,43 @@
+let basicJsFundamentalsTopic = document.querySelector(
+  ".basicJsFundamentalsNav"
+);
+console.log(basicJsFundamentalsTopic);
+
+let allTopNavItems = document.querySelectorAll(".basicJsFundamentalsNav");
+console.log(allTopNavItems);
+let allSectionChallengesItems = document.querySelectorAll(
+  ".sectionChallengesItem"
+);
+console.log(allSectionChallengesItems);
 let allNavItems = document.querySelectorAll(".navStartersJsChallenges");
 console.log(allNavItems);
 let allChallengesItems = document.querySelectorAll(".challengeSection");
-// console.log(allChallengesItems);
+console.log(allChallengesItems);
 
-let showChallenge = (event) => {
+let showSection = (event) => {
+  console.log("+");
+};
+// let clickedTopNavItem = event.target;
+// let clickedNavItemId = clickedTopNavItem.id;
+// console.log(clickedNavItemId);
+
+//   allSectionChallengesItems.forEach((sectionItem) => {
+//     // console.log(challengesItem);
+//     if (clickedNavItemId.toLowerCase() === sectionItem.id.toLowerCase()) {
+//       console.log("+");
+//       // if (challengesItem.classList.contains("startersJsChallengesHidden")) {
+//       //   challengesItem.classList.add("startersJsChallengesVisible");
+//       //   challengesItem.classList.remove("startersJsChallengesHidden");
+//       // } else {
+//       //   challengesItem.classList.add("startersJsChallengesHidden");
+//       //   challengesItem.classList.remove("startersJsChallengesVisible");
+//       // }
+//       // challengesItem.classList.remove("startersJsChallengesVisible");
+//     }
+//   });
+// };
+
+let showCheatSheet = (event) => {
   let clickedNavItem = event.target;
   let clickedNavItemId = clickedNavItem.id.substring(3);
 
@@ -22,4 +56,5 @@ let showChallenge = (event) => {
   });
 };
 
-navCheatSheet.addEventListener("click", showChallenge);
+navCheatSheet.addEventListener("click", showCheatSheet);
+basicJsFundamentalsTopic.addEventListener("click", showSection);
