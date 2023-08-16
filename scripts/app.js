9260046680b6b3478aef8f4d10374320ce774f40
GreenCatInForest <event2016.2017@gmail.com>
2023-08-16 20:36:32 +0100

    polish nav js

--- scripts/app.js
+++ scripts/app.js
@@ -26,12 +26,31 @@ let showSection = (event) => {
   console.log("+");
 
   allSectionChallengesItems.forEach((allSectionChallengesItem) => {
+    console.log(allSectionChallengesItem);
     if (allSectionChallengesItem.className.includes(clickedNavItemId)) {
-      console.log("++");
       allSectionChallengesItem.classList.add("basicJsFundamentalsTopicVisible");
       allSectionChallengesItem.classList.remove(
         "basicJsFundamentalsTopicHidden"
       );
+
+      // if (
+      //   allSectionChallengesItem.classList.contains(
+      //     "basicJsFundamentalsTopicHidden"
+      //   )
+      // ) {
+      //   console.log("++");
+      //   allSectionChallengesItem.classList.add(
+      //     "basicJsFundamentalsTopicVisible"
+      //   );
+      //   allSectionChallengesItem.classList.remove(
+      //     "basicJsFundamentalsTopicHidden"
+      //   );
+      // } else {
+      //   allSectionChallengesItem.classList.add("startersJsChallengesHidden");
+      //   allSectionChallengesItem.classList.remove(
+      //     "basicJsFundamentalsTopicVisible"
+      //   );
+      // }
     } else if (!allSectionChallengesItem.className.includes(clickedNavItemId)) {
       console.log("--");
       allSectionChallengesItem.classList.add("basicJsFundamentalsTopicHidden");
@@ -39,34 +58,31 @@ let showSection = (event) => {
         "basicJsFundamentalsTopicVisible"
       );
     }
-    allSectionChallengesItem.classList.remove(
-      "basicJsFundamentalsTopicVisible"
-    );
   });
 };
 
 // show cheat sheets
 
-let showCheatSheet = (event) => {
-  let clickedNavItem = event.target;
-  let clickedNavItemId = clickedNavItem.id.substring(3);
+// let showCheatSheet = (event) => {
+//   let clickedNavItem = event.target;
+//   let clickedNavItemId = clickedNavItem.id.substring(3);
 
-  allChallengesItems.forEach((challengesItem) => {
-    console.log(challengesItem);
-    console.log(clickedNavItemId);
-    console.log(challengesItem.id);
-    // if (clickedNavItemId.toLowerCase() === challengesItem.id.toLowerCase()) {
-    //   if (challengesItem.classList.contains("startersJsChallengesHidden")) {
-    //     challengesItem.classList.add("startersJsChallengesVisible");
-    //     challengesItem.classList.remove("startersJsChallengesHidden");
-    //   } else {
-    //     challengesItem.classList.add("startersJsChallengesHidden");
-    //     challengesItem.classList.remove("startersJsChallengesVisible");
-    //   }
-    //   challengesItem.classList.remove("startersJsChallengesVisible");
-    // }
-  });
-};
+//   allChallengesItems.forEach((challengesItem) => {
+//     console.log(challengesItem);
+//     console.log(clickedNavItemId);
+//     console.log(challengesItem.id);
+// if (clickedNavItemId.toLowerCase() === challengesItem.id.toLowerCase()) {
+//   if (challengesItem.classList.contains("startersJsChallengesHidden")) {
+//     challengesItem.classList.add("startersJsChallengesVisible");
+//     challengesItem.classList.remove("startersJsChallengesHidden");
+//   } else {
+//     challengesItem.classList.add("startersJsChallengesHidden");
+//     challengesItem.classList.remove("startersJsChallengesVisible");
+//   }
+//   challengesItem.classList.remove("startersJsChallengesVisible");
+// }
+//   });
+// };
 
 navCheatSheets.forEach((navCheatSheet) =>
   navCheatSheet.addEventListener("click", console.log("+"))
