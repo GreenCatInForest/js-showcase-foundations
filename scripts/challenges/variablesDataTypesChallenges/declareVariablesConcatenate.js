de09727bd38323a9c13eb474a8892da263abea5a
GreenCatInForest <event2016.2017@gmail.com>
2023-08-24 00:02:04 +0100

    add challenge boilerplate

--- scripts/challenges/variablesDataTypesChallenges/declareVariablesConcatenate.js
+++ scripts/challenges/variablesDataTypesChallenges/declareVariablesConcatenate.js
@@ -1,4 +1,11 @@
-let test = () => {
-  console.log("+");
-};
-test();
+addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let firstValue = document.querySelector("#dataTypeFirst").value;
+  let secondValue = document.querySelector("#dataTypeSecond").value;
+
+  console.log(typeof firstValue);
+  console.log(typeof secondValue);
+
+  console.log(firstValue + secondValue);
+});
